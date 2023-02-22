e8ee4fc71590193fa0c558a9b652aaf4e9db4bb0
NafisFuadTanvir <nafisf026@gmail.com>
2023-02-22 11:10:39 +0600

    up

--- reactpractise-2/src/App.js
+++ reactpractise-2/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
@@ -10,12 +10,49 @@ function App() {
   return (
     <div className="App">
    
-    <Counter></Counter>
+   <ExternalUsers></ExternalUsers>
       
     </div>
   );
 }
 
+
+
+function ExternalUsers(){
+
+  const [users,setUsers]= useState([]);
+  useEffect(()=>{
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res=>res.json())
+  .then(data=>setUsers(data))
+
+  },[])
+
+return (
+<div>
+  <h2>External Users</h2>
+   {
+    users.map(user=><User name={user.name} email={user.email}></User>)
+   }
+</div>
+
+)
+
+}
+
+function User(props){
+
+  return (
+   <div style={{border:'2px solid gray',margin:"20px",padding:"20px"}}>
+    <h3>Name:-{props.name}</h3>
+    <p>Email:- {props.email}</p>
+   </div>
+ 
+
+  )
+
+}
 
 
 function Counter(){
